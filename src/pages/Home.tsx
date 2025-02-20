cb7266ec9293623007422a36675fecf2f753f0e3
oyewosolomon <oyewosolomon@gmail.com>
2025-02-20 17:28:29 +0100

    Case study removed

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,9 +1,5 @@
 import Hero from "@/components/sections/Hero";
 import Features from "@/components/sections/Features";
-import Analytics from "@/components/sections/Analytics";
-import Success from "@/components/sections/Success";
-import Team from "@/components/sections/Team";
-import Contact from "@/components/sections/Contact";
 import Footer from "@/components/layout/Footer";
 import Testimonials from "@/components/sections/Testimonials";
 
